ba8154dd923fd8286e669a4d46844c9b572f6465
badman27 <30854558+badman27@users.noreply.github.com>
2018-05-04 13:26:57 -0700

    Initial Commit

---
+++ WatsonSeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatsonSeeFood
+//
+//  Created by Neema Badihian on 5/4/18.
+//  Copyright © 2018 Neema Badihian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
