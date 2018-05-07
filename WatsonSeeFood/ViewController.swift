eade9a30d7bea4373ac21f45cc2b453a6da2c42a
badman27 <30854558+badman27@users.noreply.github.com>
2018-05-07 02:13:18 -0700

    Complete Project. Use IBM Watson to test if image is hotdog or not.

--- WatsonSeeFood/ViewController.swift
+++ WatsonSeeFood/ViewController.swift
@@ -7,19 +7,111 @@
 //
 
 import UIKit
+import VisualRecognitionV3
+import SVProgressHUD
+import Social
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let apiKey = "79c83a57595203c1245ad80960966253cb885454"
+    let version = "2018-05-04"
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topBarImageView: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    var classificationResults : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        shareButton.isHidden = true
+        
+        imagePicker.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        cameraButton.isEnabled = false
+        SVProgressHUD.show()
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(apiKey: apiKey, version: version)
+            
+            //            let imageData = UIImageJPEGRepresentation(image, 0.01)
+            //
+            //            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            //
+            //            let fileURL = documentsURL.appendingPathComponent("tempImage.jpg")
+            //
+            //            try? imageData?.write(to: fileURL, options: [])
+            
+            visualRecognition.classify(image: image) { (classifiedImages) in
+                //                print(classifiedImages)
+                let classes = classifiedImages.images.first!.classifiers.first!.classes
+                
+                self.classificationResults = []
+                
+                for index in 0..<classes.count {
+                    self.classificationResults.append(classes[index].className)
+                }
+                print(self.classificationResults)
+                
+                DispatchQueue.main.async {
+                    self.cameraButton.isEnabled = true
+                    SVProgressHUD.dismiss()
+//                    self.shareButton.isHidden = false
+                }
+                
+                if self.classificationResults.contains("hotdog"){
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.green
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.image = UIImage(named: "hotdog")
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Not Hotdog!"
+                        self.navigationController?.navigationBar.barTintColor = UIColor.red
+                        self.navigationController?.navigationBar.isTranslucent = false
+                        self.topBarImageView.image = UIImage(named: "not-hotdog")
+                    }
+                }
+            }
+            
+        } else {
+            print("There was an error picking the image")
+        }
+        
     }
-
-
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func shareTapped(_ sender: UIButton) {
+        
+        if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter) {
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            vc?.setInitialText("My food is \(navigationItem.title) 😮")
+            vc?.add(#imageLiteral(resourceName: "hotdogBackground"))
+            present(vc!, animated: true, completion: nil)
+        } else {
+            self.navigationItem.title = "Please Log In to Twitter"
+        }
+    }
+    
 }
 
